3288632b89f5d6e133b2c4269a65b74d65a46a85
rizaldiabyannata <aldizar255@gmail.com>
2024-12-04 22:24:28 +0800

    class: add class HpBar, HpDisplayPanel

--- src/main/java/com/main/HpDisplayPanel.java
+++ src/main/java/com/main/HpDisplayPanel.java
@@ -16,12 +16,6 @@ public class HpDisplayPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        // Gambar HP Bar Wizard (kiri atas)
-        wizardPanel.getWizard().getHp().draw(g, 20, 20, 200, 20);
-
-        // Gambar HP Bar Monster (kanan atas)
-        monsterPanel.getMonster().getHp().draw(g, 1146, 20, 200, 20);
+       
     }
 }
